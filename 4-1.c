001238f7cd415f8bfd7b6269b51b7a209839d8a4
2536566061 <2536566061@qq.com>
2017-11-19 19:23:30 +0800

    C语言

---
+++ 4-1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<float.h>
+
+int main()
+{
+	printf("The size of smallest positive non-zero value of type float is %.3e\n",DBL_MIN);
+	printf("The size of largest value of type float is %.3e\n",DBL_MAX);
+	return 0;
+}
