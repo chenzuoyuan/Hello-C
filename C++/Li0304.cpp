a211ad22373885ea3bbc664aece36a3ff5081057
ZuoYuan <2536566061@qq.com>
2018-07-11 21:05:36 +0800

    Create Li0304.cpp

---
+++ C++/Li0304.cpp
@@ -0,0 +1,8 @@
+#include <iostream.h>
+void main( ){
+    int a = 10, b = 20, c = 30, d = 40 ;
+    double m = 5.23, n = 100 ;
+    cout << a << b << endl ;
+    cout << c << n-d << endl ;
+    cout << m << n << endl ;
+}
