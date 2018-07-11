6bac2664cf5e613dccaa4571268a926a51d98ae7
ZuoYuan <2536566061@qq.com>
2018-07-11 17:08:19 +0800

    Update Li0101.cpp

--- C++/Li0101.cpp
+++ C++/Li0101.cpp
@@ -1,1 +1,8 @@
-#include
+#include <iostream.h>
+void main ( void ){
+	int num, square ;
+	cout << "num=" ;
+	cin >> num ;
+	square = num * num ;
+	cout << " the square of num is:" << square << '\n' ;
+}
