9f295075666ee523737d6bb0dba8a5188ec3a34d
ZuoYuan <2536566061@qq.com>
2018-07-11 20:34:29 +0800

    Create Li0201.cpp

---
+++ C++/Li0201.cpp
@@ -0,0 +1,13 @@
+#include <iostream.h>
+#define PI 3.14159
+void main( void ){
+    float r, c, a, sa ;
+    r = 5.0f ;
+    c = 2 * PI * r ;
+    a = PI * r *r ;
+    sa = 4 * PI * r * r ;
+    cout << "PI = " << PI << '\n' ;
+    cout << "c = " << c << '\n' ;
+    cout << "a = " << a << '\n' ;
+    cout << "sa =" << sa << '\n' ;
+}
