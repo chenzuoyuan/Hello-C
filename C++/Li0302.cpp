973b08e2fbc8b9f699c4b67c3820c19c33183968
ZuoYuan <2536566061@qq.com>
2018-07-11 21:20:29 +0800

    Create Li0302.cpp

---
+++ C++/Li0302.cpp
@@ -0,0 +1,7 @@
+#include <iostream.h>  //The use of input and output in C++
+void main( ){
+    char a ;
+    cout << "Please input a character :" ;
+    cin >> a ;
+    cout << "The character is :" << a << endl ;
+}
